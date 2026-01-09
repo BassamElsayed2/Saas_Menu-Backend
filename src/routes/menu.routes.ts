58d2de19e9698d2f41a9a573218730be9ba2c6de
Bassam-Elsaied <93853900+Bassam-Elsaied@users.noreply.github.com>
2026-01-09 23:43:44 +0200

    add 2 temp

--- src/routes/menu.routes.ts
+++ src/routes/menu.routes.ts
@@ -38,9 +38,7 @@ router.post(
     body("descriptionEn").optional().isString().trim().isLength({ max: 1000 }),
     body("slug").optional().isString().trim().isLength({ max: 200 }),
     body("logo").optional().isString().isLength({ max: 500 }),
-    body("theme")
-      .optional()
-      .isIn(["default", "neon", "modern", "minimal", "template2", "template3"]),
+    body("theme").optional().isIn(["default", "neon", "coffee", "template1"]),
   ]),
   menuController.createMenu
 );
@@ -75,9 +73,7 @@ router.put(
       .optional({ nullable: true, checkFalsy: true })
       .isString()
       .isLength({ max: 500 }),
-    body("theme")
-      .optional()
-      .isIn(["default", "neon", "modern", "minimal", "template2", "template3"]),
+    body("theme").optional().isIn(["default", "neon", "coffee", "template1"]),
     body("currency").optional().isString().isLength({ min: 3, max: 3 }),
     body("isActive").optional().isBoolean(),
   ]),
